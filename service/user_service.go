2f6a0766e9303b7323a958f8c837a769b21f1973
bruce0hh <bruce0hh@gmail.com>
2022-03-21 23:54:37 +0800

    fix project struct

--- service/user_service.go
+++ service/user_service.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"log"
-	"regexp"
 )
 
 var (
@@ -48,18 +47,6 @@ func DelToken(id string, ctx context.Context) {
 		return
 	}
 	rdb.Del(ctx, user.Username)
-}
-
-//CheckPhone 校验手机号
-func CheckPhone(phone string) bool {
-	phoneReg, _ := regexp.Match("^1(3\\d{2}|4[14-9]\\d|5([0-35689]\\d|7[1-79])|66\\d|7[2-35-8]\\d|8\\d{2}|9[13589]\\d)\\d{7}$", []byte(phone))
-	if len(phone) != 11 { //判断手机号是否为11位
-		return false
-	} else if !phoneReg { //判断手机号是否符合正则
-		return false
-	}
-
-	return true
 }
 
 //IsPhoneExist 判断电话号码是否存在
